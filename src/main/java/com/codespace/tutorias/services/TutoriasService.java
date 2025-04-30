dbc397033cf678b97f80ecf049bc7875b8894150
Shtven <gael1239983@gmail.com>
2025-04-30 12:42:59 -0600

    add controller and solve errors

--- src/main/java/com/codespace/tutorias/services/TutoriasService.java
+++ src/main/java/com/codespace/tutorias/services/TutoriasService.java
@@ -3,10 +3,12 @@ package com.codespace.tutorias.services;
 import com.codespace.tutorias.models.Tutoria;
 import com.codespace.tutorias.repository.TutoriasRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class TutoriasService {
     @Autowired
     private TutoriasRepository tutoriasRepository;
