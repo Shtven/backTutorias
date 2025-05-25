3b7c54721162e32d9f979245ab6426526738d52e
Shtven <gael1239983@gmail.com>
2025-05-25 12:47:06 -0600

    cambios finales3.2

--- src/main/java/com/codespace/tutorias/repository/TutoriasRepository.java
+++ src/main/java/com/codespace/tutorias/repository/TutoriasRepository.java
@@ -12,9 +12,6 @@ import java.util.List;
 
 public interface TutoriasRepository extends JpaRepository<Tutoria, Integer> {
 
-    @Query("SELECT t FROM Tutoria t JOIN t.tutorados tut WHERE tut.matricula = :matricula")
-    List<Tutoria> findTutoriasPorTutorado(String matricula);
-
     @Query("SELECT t from Tutoria t JOIN t.horario h JOIN h.tutor tut WHERE tut.matricula = :matricula")
     List<Tutoria> findTutoriasPorTutor(String matricula);
 
@@ -23,9 +20,5 @@ public interface TutoriasRepository extends JpaRepository<Tutoria, Integer> {
 
     @Query("SELECT COUNT(t) FROM Tutoria t")
     long countTotalTutorias();
-
-    @Query("SELECT COUNT(DISTINCT tut) FROM Tutoria t JOIN t.tutorados tut")
-    long countTotalAlumnosInscritos();
-
 
 }
