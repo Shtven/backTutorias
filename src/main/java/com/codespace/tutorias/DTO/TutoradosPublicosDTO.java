2e073c1c053b13aa1122eb4815fce51eae30119e
Shtven <gael1239983@gmail.com>
2025-05-29 12:21:48 -0600

    pinche Wicho, me cagas

--- src/main/java/com/codespace/tutorias/DTO/TutoradosPublicosDTO.java
+++ src/main/java/com/codespace/tutorias/DTO/TutoradosPublicosDTO.java
@@ -1,39 +1,51 @@
 package com.codespace.tutorias.DTO;
 
 public class TutoradosPublicosDTO {
-        private String matricula;
-        private String nombre;
-        private String correo;
+    private String matricula;
+    private String nombre;
+    private String apellidoP;
+    private String apellidoM;
+    private String correo;
 
-        public TutoradosPublicosDTO(){}
-
-    public TutoradosPublicosDTO(String matricula, String nombre, String correo) {
-        this.matricula = matricula;
-        this.nombre = nombre;
-        this.correo = correo;
-    }
+    public TutoradosPublicosDTO(){}
 
     public String getMatricula() {
-            return matricula;
-        }
+        return matricula;
+    }
 
-        public void setMatricula(String matricula) {
-            this.matricula = matricula;
-        }
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
 
-        public String getNombre() {
-            return nombre;
-        }
+    public String getNombre() {
+        return nombre;
+    }
 
-        public void setNombre(String nombre) {
-            this.nombre = nombre;
-        }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-        public String getCorreo() {
-            return correo;
-        }
+    public String getApellidoP() {
+        return apellidoP;
+    }
 
-        public void setCorreo(String correo) {
-            this.correo = correo;
-        }
+    public void setApellidoP(String apellidoP) {
+        this.apellidoP = apellidoP;
+    }
+
+    public String getApellidoM() {
+        return apellidoM;
+    }
+
+    public void setApellidoM(String apellidoM) {
+        this.apellidoM = apellidoM;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
 }
